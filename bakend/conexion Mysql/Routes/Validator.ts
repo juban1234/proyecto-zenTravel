973779b60523f386c31567640d4bb185cd176af8
nicolas20055 <ortiznicolas656@gmail.com>
2025-05-12 13:34:16 -0500

    act

--- bakend/conexion Mysql/Routes/Validator.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { validatePassword,resetPassword } from '../controllers/controlerUser/ValidatoPassword';
-
-const router = Router();
-
-router.post('/validar-password', validatePassword); //funcional
-router.post('/reset-password', resetPassword); // funcional
-
-export default router;

88c12f6eb5f04d51a3e30872301dd954bab21f00
juan esteban grajales <alexzo8677@gmail.com>
2025-05-30 15:03:38 -0500

    actualizacion 2:54 30/05

--- repositories/paqueteRepo.ts
+++ repositories/paqueteRepo.ts
@@ -118,11 +118,13 @@ class Paquetes {
             return "no se a actualizado ningun campo"
         }
 
+        const sql = `UPDATE PAQUETE SET ${updates.join(", ")} WHERE id_paquete = ?`
+
         // Agregar timestamp de actualización
         updates.push("updated_at = CURRENT_TIMESTAMP")
         values.push(id_usuario) // Para el WHERE
 
-        const sql = `UPDATE PAQUETE SET ${updates.join(", ")} WHERE id_paquete = ?`
+        
 
         const [rows]: any = await db.execute(sql)
 

6ac91c0ff9c57eeb187e23f4d2999198c05b0bd5
juan esteban grajales <alexzo8677@gmail.com>
2025-06-04 15:16:49 -0500

    actualizacion 3:16

--- middleware/validatorRol.ts
+++ middleware/validatorRol.ts
@@ -4,6 +4,8 @@ import jwt from 'jsonwebtoken';
 export const verificarRol = (...rolesPermitidos: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
+    console.log(authHeader);
+    
 
     if (!authHeader) {
       return res.status(401).json({ error: 'Token no proporcionado' });
@@ -14,11 +16,14 @@ export const verificarRol = (...rolesPermitidos: string[]) => {
     try {
       const decoded = jwt.verify(token, process.env.KEY_TOKEN as string) as { id: number; rol: string };
 
+      console.log("rol: ",decoded.rol );
+      
+
       if (!rolesPermitidos.includes(decoded.rol)) {
         return res.status(403).json({ error: 'Acceso denegado: rol no autorizado' });
       }
 
-      (req as any).user = { id: decoded.id, rol: decoded.rol };
+      (req as any).user = decoded.id;
       console.log("✅ Token decodificado:", decoded);
 
       next();

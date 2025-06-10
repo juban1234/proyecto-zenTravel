a971d4c23ed6674f8964205a9b2a013973fb2479
juan esteban grajales <alexzo8677@gmail.com>
2025-06-10 16:19:02 -0500

    actualizaion 4:18

--- configs/config.ts
+++ configs/config.ts
@@ -10,7 +10,9 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT) || 3306,
-
+  ssl: {
+    ca: fs.readFileSync(path.resolve(__dirname, '../../certs/DigiCertGlobalRootCA.crt.pem'))
+  },
 
 }).promise();
 

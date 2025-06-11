221b0763741c8593039a75a33348fddd5736af91
juan esteban grajales <alexzo8677@gmail.com>
2025-06-11 15:33:57 -0500

    actualizacion

--- Routes/administrar.ts
+++ Routes/administrar.ts
@@ -8,21 +8,21 @@ import { validatorParams } from "../middleware/register-validator";
 const router = express.Router();
 
 // control de usuarios del sistema
-router.patch('User/EditeRol', verificarRol("admin"),RolUsuario); //funcional
-router.delete('/UserDelete/:nombre',verificarRol('admin'),EliminarUsuarios) //funcional
+router.patch('User/EditeRol', verificarRol('Admin'),RolUsuario); //funcional
+router.delete('/UserDelete/:nombre',verificarRol('Admin'),EliminarUsuarios) //funcional
 router.get('/Users/:Rol',verificarRol('Admin','Empleado'),TraerUsuario)
-router.post('/CreateUsers',validatorParams,verificarRol('admin'),newEmpleados)
+router.post('/CreateUsers',validatorParams,verificarRol('Admin'),newEmpleados)
 
 // control de contenido del sistema
-router.post('/CreateDestiny',verificarRol("admin",'vendedor'),createDestino);
-router.post('/CreateHotel',verificarRol("admin",'vendedor'),createHotel);
-router.post('/CreateRoom',verificarRol("admin",'vendedor'),createHabitacion);
-router.post ('/CreateTransport',verificarRol("admin",'vendedor'),createTransporte);
+router.post('/CreateDestiny',verificarRol('Admin','Empleado'),createDestino);
+router.post('/CreateHotel',verificarRol('Admin','Empleado'),createHotel);
+router.post('/CreateRoom',verificarRol('Admin','Empleado'),createHabitacion);
+router.post ('/CreateTransport',verificarRol('Admin','Empleado'),createTransporte);
 
 // eliminar contenido del sistema
-router.delete('/deletePackage/:id_paquete',verificarRol('admin'),deletePaquete);
-router.delete('/deleteDestiny/:id_destino',verificarRol('admin'),deleteDestino);
-router.delete('/deleteHotel/:id_hotel',verificarRol('admin'),deleteHotel);
-router.delete('/deleteTransport/:id_transporte',verificarRol('admin'),deleteTransporte);
+router.delete('/deletePackage/:id_paquete',verificarRol('Admin'),deletePaquete);
+router.delete('/deleteDestiny/:id_destino',verificarRol('Admin'),deleteDestino);
+router.delete('/deleteHotel/:id_hotel',verificarRol('Admin'),deleteHotel);
+router.delete('/deleteTransport/:id_transporte',verificarRol('Admin'),deleteTransporte);
 
 export default router;

7a4d7e1bbbeb1f55337dc0353999543ddd35579f
juan esteban grajales <alexzo8677@gmail.com>
2025-06-19 15:45:45 -0500

    actualizacion

--- controllers/controllerServis/EditController.ts
+++ controllers/controllerServis/EditController.ts
@@ -5,11 +5,8 @@ import upload from '../../configs/multer';
 import fs from 'fs';
 import { promisify } from 'util';
 
-<<<<<<< HEAD
-=======
 const uploadSingle = promisify(upload.single('imagen'));
 
->>>>>>> origin
 export const Editdestino = async (req: Request, res: Response) => {
   try {
     const id_destino = Number(req.params.id_destino);
@@ -63,11 +60,6 @@ export const Editdestino = async (req: Request, res: Response) => {
   }
 }
 
-<<<<<<< HEAD
-
-const uploadSingle = promisify(upload.single('imagen'));
-=======
->>>>>>> origin
 export const EditarHotel = async (req: Request, res: Response) => {
 
   try {
@@ -128,87 +120,3 @@ export const EditarHotel = async (req: Request, res: Response) => {
   }
 }
 
-<<<<<<< HEAD
-const uploadMultiple = promisify(upload.array('imagenes', 5));
-
-export const EditarHabitacion = async (req: Request, res: Response) => {
-  try {
-
-    await uploadMultiple(req, res);
-
-    const id_habitacion = Number(req.params.id_habitacion);
-    if (!id_habitacion || isNaN(id_habitacion)) {
-      return res.status(401).json({ error: "ID de habitación inválido o usuario no autenticado" });
-    }
-
-    const {
-      tipo,
-      numero,
-      precio,
-      disponible,
-      id_hotel,
-    } = req.body;
-
-    
-    const files = req.files as Express.Multer.File[];
-    if (!files || files.length === 0) {
-      return res.status(400).json({ error: 'Las imágenes son requeridas' });
-    }
-
-    
-    const imagenes = await Promise.all(
-      files.map(async (file) => {
-        const uploadedImage = await cloudinary.uploader.upload(file.path);
-        fs.unlinkSync(file.path);
-        return uploadedImage.secure_url;
-      })
-    );
-
-    const campos = {
-      tipo: tipo?.trim(),
-      numero: parseInt(numero),
-      precio: parseFloat(precio),
-      disponible: disponible.trim(),  
-      id_hotel: parseInt(id_hotel),
-    };
-
-    for (const [campo, valor] of Object.entries(campos)) {
-      if (
-        valor === undefined ||
-        valor === null ||
-        (typeof valor === 'string' && valor.trim() === '') ||
-        (typeof valor === 'number' && isNaN(valor))
-      ) {
-        return res.status(400).json({ error: `El campo '${campo}' es requerido y no puede estar vacío o inválido` });
-      }
-    }
-
-    const resultado = await usuarioRepo.EditarHabitacion(id_habitacion, {
-      tipo: campos.tipo!,
-      numero: campos.numero!,
-      precio: campos.precio!,
-      disponible: campos.disponible!,
-      imagenes,
-    });
-
-    if (!resultado) {
-      return res.status(404).json({ error: "Habitación no encontrada o no actualizada" });
-    }
-
-    return res.status(200).json({
-      status: "Habitación editada con éxito",
-      habitacion: resultado,
-    });
-
-  } catch (error: any) {
-    console.error("Error al editar la habitación:", error.message || error);
-    return res.status(500).json({ error: "Error en el servidor" });
-  }
-};
-
-    
-
-
-
-=======
->>>>>>> origin

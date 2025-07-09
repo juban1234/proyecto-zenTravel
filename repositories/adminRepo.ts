602f494ef14bb910f3d324bd40d631b763db49e5
juan esteban grajales <alexzo8677@gmail.com>
2025-07-09 13:27:11 -0500

    actualizacion

--- repositories/adminRepo.ts
+++ repositories/adminRepo.ts
@@ -144,7 +144,7 @@ class admin {
         }
     }
 
-    static async editardestino( id_destino: number, D: any) {
+    static async editardestino( id_destino: number, D: any ) {
 
     let campos: string[] = [];
     let valores: any[] = [];
@@ -208,6 +208,11 @@ class admin {
     if (D.imagenes) {
         campos.push("imagenes = ?");
         valores.push(JSON.stringify(D.imagenes));
+    }
+
+    if(D.imageHabitacion){
+        campos.push("imageneshabitaciones = ?");
+        valores.push(JSON.stringify(D.imageHabitacion));
     }
 
     if (campos.length === 0) {
